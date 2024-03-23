b6e9d5a7ad12af63f9a8fedbb17e93378cee3278
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-03-23 09:07:34 +0800

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2d00fb155d6641ac6549757279011d07287bca5b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2d00fb155d6641ac6549757279011d07287bca5b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'beycanpress/cryptopay-integrator' => array(
+            'pretty_version' => '0.1.10',
+            'version' => '0.1.10.0',
+            'reference' => '6ac799ad5b3c9b952ee77d4d974162f525ff0f2a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../beycanpress/cryptopay-integrator',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
